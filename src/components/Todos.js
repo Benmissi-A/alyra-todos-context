fc8a6edb969373f255ab4a84b17b03f5ff864f75
Benmissi-A <aminebenmissi@hotmail.fr>
2021-05-18 10:50:32 +0200

    with api

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -5,47 +5,21 @@ import AddTodoForm from "./AddTodoForm"
 import { todosReducer } from '../reducers/TodosReducer'
 import { TodosDispatchContext } from '../context/TodosDispatchContext'
 
-const initialTodos = [
-  {
-    text: "Forkez et cloner ce repo",
-    isCompleted: true,
-    id: "1b688c51-e990-4ce3-95a5-9018cf81d23d"
-  },
-  {
-    text: "Refactor todos avec useReducer",
-    isCompleted: false,
-    id: "efc6331d-7ca2-49a6-b014-378b8280b33d"
-  },
-  {
-    text: "Add dispatch to Context",
-    isCompleted: false,
-    id: "9e60d353-cd72-40bb-97e6-5841e51635c0"
-  },
-  {
-    text: "Add Color Mode Context",
-    isCompleted: false,
-    id: "df0ce18c-b4fa-4651-82c0-72fad6b486e4"
-  }
-]
+const initialState = {
+  todos: [],
+  loading: false,
+  error: "",
+}
 
 const Todos = () => {
-  const [state, dispatch] = useReducer(todosReducer, initialTodos, (initialTodos) => {
-    return JSON.parse(window.localStorage.getItem("my-new-todos")) || initialTodos
-  })
-
-
-  useEffect(() => {
-    window.localStorage.setItem("my-new-todos", JSON.stringify(state))
-  }, [state])
-
-
-
-
+  const [state, dispatch] = useReducer(todosReducer, initialState)
+  const {todos,loading, error} = state
   return (
     <main>
       <h2 className="text-center">Ma liste de tâches ({state.length})</h2>
+      {error && <p className="alert alert-danger">{error}</p>}
       <TodosDispatchContext.Provider value={dispatch}>
-        <TodosList todos={state} />
+        <TodosList todos={todos} />
         <AddTodoForm />
       </TodosDispatchContext.Provider>
     </main>
--- src/reducers/TodosReducer.js
+++ src/reducers/TodosReducer.js
@@ -1,19 +1,49 @@
+// src/reducers/todosReducer.js
 export const todosReducer = (state, action) => {
+  // ADD, DELETE, TOGGLE, FETCH_INIT, FETCH_SUCCESS, FETCH_FAILURE
   switch (action.type) {
-    case 'ADD':
-      return [...state, action.payload]
-    case 'DELETE': return state.filter((el) => el.id !== action.payload.id)
-    case 'TOGGLE':
-      return state.map((el) => {
-        if (el.id === action.payload.id) {
-          return {
-            ...el,
-            isCompleted: !el.isCompleted
+    case "ADD":
+      return {
+        ...state,
+        todos: [...state.todos, action.payload],
+      }
+    case "DELETE":
+      return {
+        ...state,
+        todos: state.todos.filter(el => el.id !== action.payload.id),
+      }
+    case "TOGGLE":
+      return {
+        ...state,
+        todos: state.todos.map(el => {
+          if (el.id === action.payload.id) {
+            return {
+              ...el,
+              isCompleted: !el.isCompleted,
+            }
           }
-        }
-        return el
-      })
+          return el
+        }),
+      }
+    case "FETCH_INIT": // 🆕
+      return {
+        ...state,
+        loading: true,
+      }
+    case "FETCH_FAILURE": // 🆕
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      }
+    case "FETCH_SUCCESS": // 🆕
+      return {
+        ...state,
+        loading: false,
+        error: "",
+        todos: action.payload,
+      }
     default:
-      throw new Error(`Unsupported action type: ${action.type}`)
+      throw new Error(`Unsupported action type ${action.type} in todosReducer`)
   }
 }
